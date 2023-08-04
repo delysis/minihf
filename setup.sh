ccc9285511867a372c86ad72733d819dfd4f55b7
George Walker <georgewalkeriv@gmail.com>
2023-08-04 13:03:42 -0400

    Flask listens on all adapters.

--- setup.sh
+++ setup.sh
@@ -4,4 +4,4 @@ apt-get install python3.10-venv -y
 python3 -m venv env_minihf
 source env_minihf/bin/activate
 pip3 install -r requirements.txt 
-flask --app minihf_infer run
+flask --app minihf_infer run -h 0.0.0.0
